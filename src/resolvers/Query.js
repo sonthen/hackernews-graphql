a4bf694a9626dec3aefd268c40c79ccecc6a55c4
Jason Thenneil <sonthen@gmail.com>
2019-01-07 12:33:56 +0700

    apply filter, apply delete

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,17 @@
-function feed(parent, args, context) {
-  return context.prisma.links();
+async function feed(parent, args, context) {
+  const where = args.filter
+    ? {
+      OR: [
+        {description_contains: args.filter},
+        {url_contains: args.filter},
+      ],
+    }
+    : {};
+
+  const links = await context.prisma.links({
+    where,
+  });
+  return links;
 }
 
 export {feed};
